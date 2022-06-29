de42fca6457669775f2c69d6f536433218cf2924
Sebastian Knell <sebastianknell@hotmail.com>
2022-06-29 10:47:18 -0500

    insert helper functions

--- SSTree.cpp
+++ SSTree.cpp
@@ -10,6 +10,56 @@ Node::~Node() {
     }
 }
 
+double getDistance(Point a, Point b) {
+    double distance = 0.0;
+    for (int i = 0; i < DIM; i++)
+        distance += pow(a[i] - b[i], 2);
+    return sqrt(distance);
+}
+
+static double getVariance(vector<Point> points, int direction) {
+    double mean = 0;
+    for (auto &p : points) mean += p[direction];
+    mean /= points.size();
+    double variance = 0;
+    for (auto &p : points) variance += pow(p[direction] - mean, 2);
+    return variance / points.size();
+}
+
+static vector<Point> getCentroids(Node* node) {
+    if (node->isLeaf) return node->points;
+    vector<Point> centroids;
+    for (auto &c : node->childs)
+        centroids.push_back(c->circle.center);
+    return centroids;
+}
+
+static Node* findClosestChild(Node* node, Point p) {
+    assert(!node->isLeaf);
+    double min = INT_MAX;
+    Node* result = nullptr;
+    for (auto &c : node->childs) {
+        auto distance = getDistance(c->circle.center, p);
+        if (distance < min) {
+            min = distance;
+            result = c;
+        }
+    }
+    return result;
+}
+
+int getMaxVarianceDirection(Node* node) {
+    double maxVariance = 0;
+    int direction = 0;
+    auto centroids = getCentroids(node);
+    for (int i = 0; i < DIM; i++) {
+        auto variance = getVariance(centroids, i);
+        if (variance > maxVariance) maxVariance = variance;
+        direction = i;
+    }
+    return direction;
+}
+
 void SSTree::insert(Point point) {
 
 }
--- SSTree.h
+++ SSTree.h
@@ -13,18 +13,21 @@
 #include <random>
 #include <opencv2/opencv.hpp>
 
+#ifndef DIM
+#define DIM 2
+#endif
+
 using namespace std;
 //using Point = cv::Point;
-using Point = struct {int x; int y;};
+using Point = vector<double>;
 using Circle = struct {Point center; int radius;};
 
 struct Node {
     bool isLeaf;
     Circle circle;
     int minRadius;
-    vector<Circle> regions; // regiones o bounding circles si es hoja
-    vector<Node*> childs;
-    vector<Point> data; // valido si es hoja
+    vector<Node*> childs; // valido si no es hoja
+    vector<Point> points; // valido si es hoja
     explicit Node(bool isLeaf): isLeaf(isLeaf) {};
     ~Node();
 };
